fd2b5c3053037b8342db860eb81b2f16b3f6d49a
Cristian Espinoza <Cristian Espinoza>
2017-08-14 23:02:39 -0500

    cambio 2

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+   res.render('entradaextranjeros'); 
+    //res.send('respond with a resource sdgfhsgdhsgfhgsdfgshdgfhgsdhfghdhsdghfghd');
 });
+
 
 module.exports = router;
